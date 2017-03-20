8bf7ad1c0638f2ae4dae3963bd17747c5c6a8f63
ravi7522 <ravi.patel070597@gmail.com>
2017-03-21 00:00:25 +0530

    Delete em_ppca.m

--- submission 2/em_ppca.m
+++
@@ -1,9 +0,0 @@
-    filename = 'data.dat';
-    T = importdata(filename);
-    q = 2;
-    plot(T);
-    [W, sigma] = em_ppca_from_data(T, q);
-    X = ppca_latent(T, W, sigma);
-    figure;
-    length(X)
-    plot(X);
